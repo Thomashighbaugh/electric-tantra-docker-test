e036a392264fd699c2959275846abac7425278a7
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2020-12-13 20:58:41 -0800

    tuff stuff

---
+++ build.sh
@@ -0,0 +1,9 @@
+# Deprecated apparently, ultimately somewhat useless considering you can just run mkarchiso and it does all of what
+## these scripts did, included for reference purposes for anyone else using this repo later expecting to see these 
+# scripts - TLH
+
+echo "Good news and bad news"
+echo "Bad news, these scripts are deprecated."
+echo "Good news, the reason is because you don't need them! mkarchiso will do all that for you!"
+echo "Example Usage (command that generates Electric Tantra Linux)"
+echo "# mkarchiso -v -w /tmp/mkarchiso ./"
